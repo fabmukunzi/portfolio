4db7e3e1f68e85ccebe6d60e47f3497e9c1457de
Fabrice Mukunzi <83646585+fabmukunzi@users.noreply.github.com>
2025-03-04 14:28:52 +0200

    fixed build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,11 +10,10 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-R8HG0TSW99"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-R8HG0TSW99');
