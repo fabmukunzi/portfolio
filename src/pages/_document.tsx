c39adfb2b0253470f7ffa6c81e98e52058a5508b
Fabrice Mukunzi <83646585+fabmukunzi@users.noreply.github.com>
2025-03-04 17:45:50 +0530

    [chore]: add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,15 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-R8HG0TSW99"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-R8HG0TSW99');
+</script>
     </Html>
   );
 }
